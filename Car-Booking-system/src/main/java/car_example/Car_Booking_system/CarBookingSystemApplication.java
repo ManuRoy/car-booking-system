1591a20e5020f14bce5cab273c3ec20e863880ed
ManuRoy <manuroy62@gmail.com>
2025-09-07 08:59:02 +0530

    chore(init): initial Spring Boot setup

---
+++ Car-Booking-system/src/main/java/car_example/Car_Booking_system/CarBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package car_example.Car_Booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBookingSystemApplication.class, args);
+	}
+
+}
---
+++ Car-Booking-system/src/test/java/car_example/Car_Booking_system/CarBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package car_example.Car_Booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
